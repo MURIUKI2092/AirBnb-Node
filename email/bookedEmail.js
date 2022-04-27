d0cd0de1dd7248494072e6a0cc2fbee9dd23899e
MURIUKI2092 <muriukijames33@gmail.com>
2022-04-27 12:26:51 +0300

    fixed #001

--- email/bookedEmail.js
+++ email/bookedEmail.js
@@ -1,35 +1,39 @@
 const  nodemailer = require("nodemailer");
+const router = require("express").Router();
 
-const sendBookedEmail =(Email,hostLocation,houseTitle,username)=>{
-  let transporter = nodemailer.createTransport({
+let transporter = nodemailer.createTransport({
     service:'gmail',
     auth:{
       user: "muriukijames33@gmail.com",
       pass:"xvnuqrtdghprspap"
     }
   })
+router.post("/book",(req,res)=>{
+    let mailOptions = {
+      from:"muriukijames33@gmail.com",
+      to:req.body.to,
+      subject:"Booked",
+      // text:"A sample email to indicate that the code is working"
+      html: `<html>
+      <body>Hello ${req.body.username} ,<br></br> You have successfully booked a 
+      ${req.body.houseTitle}  in  ${req.body.hostLocation} .<br></br>Regards <strong>
+       AirBnb</strong>.</body>
+       </html>`
+    };
+    
+    transporter.sendMail(mailOptions,(err,success)=>{
+      if(err){
+        console.log(err)
+      }
+      else{
+        console.log("Email is sent")
+      }
+    
+    
   
-  let mailOptions = {
-    from:"muriukijames33@gmail.com",
-    to:`${Email}`,
-    subject:"Booked",
-    // text:"A sample email to indicate that the code is working"
-    html: `<html>
-    <body>Hello ${username} ,<br></br> You have successfully booked a 
-    ${houseTitle}  in  ${hostLocation} .<br></br>Regards <strong>
-     AirBnb</strong>.</body>
-     </html>`
-  };
-  
-  transporter.sendMail(mailOptions,(err,success)=>{
-    if(err){
-      console.log(err)
-    }
-    else{
-      console.log("Email is sent")
-    }
-  
-  })
+  }
+    )}
+)
 
-}
-module.exports =sendBookedEmail
+  
+module.exports =router;
